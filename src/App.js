b887eb72ce7a46befe35b978fac9df8d4834915c
HaidesAidoseus <80040680+HaidesAidoseus@users.noreply.github.com>
2021-03-23 11:42:40 +0300

    lesson 19 finished (not homework)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
 import Dialogs from './Components/Dialogs/Dialogs';
 import Header from './Components/Header/Header';
@@ -7,14 +8,16 @@ import Profile from './Components/Profile/Profile';
 
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Navbar /> 
-      <div className='app-wrapper-content'>
-      <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path="/dialogs" component={Dialogs} />
+          <Route path="/profile" component={Profile} />
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>);
+    </BrowserRouter>);
 }
 
 
