02c82cab5c8478412c7a8d9ad4ebdf9eb691b46e
HaidesAidoseus <80040680+HaidesAidoseus@users.noreply.github.com>
2021-03-23 14:38:02 +0300

    Homework lesson 19 finished

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Dialogs from './Components/Dialogs/Dialogs';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
+import News from './Components/News/News';
+import Music from './Components/Music/Music';
+import Settings from './Components/Settings/Settings';
 
 const App = () => {
   return (
@@ -15,6 +18,9 @@ const App = () => {
         <div className='app-wrapper-content'>
           <Route path="/dialogs" component={Dialogs} />
           <Route path="/profile" component={Profile} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
         </div>
       </div>
     </BrowserRouter>);
