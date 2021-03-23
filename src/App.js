ba20a3c7a23fc42eab8d23b2ff386e93bf8ea6c9
HaidesAidoseus <80040680+HaidesAidoseus@users.noreply.github.com>
2021-03-23 11:14:41 +0300

    Finished 18 lesson

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Dialogs from './Components/Dialogs/Dialogs';
 import Header from './Components/Header/Header';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
@@ -8,9 +9,14 @@ const App = () => {
   return (
     <div className='app-wrapper'>
       <Header />
-      <Navbar />
-      <Profile />
+      <Navbar /> 
+      <div className='app-wrapper-content'>
+      <Dialogs />
+      </div>
+      {/* <Profile /> */}
     </div>);
 }
+
+
 
 export default App;
